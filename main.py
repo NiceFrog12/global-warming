e587fa72eb23ae46453710ff27246afd5a960e6b
NiceFrog12 <156522928+NiceFrog12@users.noreply.github.com>
2024-08-10 15:44:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 ################
 
 
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -26,8 +26,7 @@ class Facts(db.Model):
     def __repr__(self):
         return f'<Fact {self.id}>'
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
 
 
 
@@ -36,3 +35,51 @@ def create_app():
 
 def get_db():
     return db
+
+
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+    
+@app.route('/browse')
+def browsing():
+    facts = Facts.query.order_by(Facts.id).all()
+    return render_template('index.html', facts=facts)
+
+
+#Запуск страницы c картой
+@app.route('/card/<int:id>')
+def fact(id):
+    fact = Facts.query.get(id)
+    return render_template('card.html', fact=fact)
+
+
+@app.route('/create')
+def create():
+    return render_template('create_card.html')
+
+#Форма карты
+@app.route('/form_create', methods=['GET','POST'])
+def form_create():
+    if request.method == 'POST':
+        title =  request.form['title']
+        subtitle =  request.form['subtitle']
+        text =  request.form['text']
+        #Создание объкта для передачи в дб
+
+        fact = Facts(title=title, subtitle=subtitle, text=text)
+
+        db.session.add(fact)
+        db.session.commit()
+        return redirect('/index')
+    else:
+        return render_template('create_card.html')
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
