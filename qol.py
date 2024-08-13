57680dfc330d9e7e5508f2f910c1227b4e3d996b
NiceFrog12 <156522928+NiceFrog12@users.noreply.github.com>
2024-08-13 14:17:20 +0200

    Update qol.py

--- qol.py
+++ qol.py
@@ -1,12 +1,6 @@
-from sqlalchemy import create_engine, func
-import random #use random to get the random id number to get the query by
 from main import db,Facts, app #to know which database im working with
-
-#making the engine for pandas
-engine = create_engine('sqlite:///instance/facts.db')
-
-#allowing pandas read through the database
-
+import random
+from sqlalchemy import func
 
 def random_fact_choice(language):
     with app.app_context():
@@ -16,7 +10,16 @@ def random_fact_choice(language):
         if min_id is None or max_id is None:
             return None
 
-        random_id = random.randint(min_id, max_id)
-        fact = db.session.query(Facts).filter(Facts.id == random_id, Facts.language == language).first()
+        attempts = 0
+        max_attempts = 10  # Limit the number of attempts to avoid potential infinite loops
 
-        return fact
+        while attempts < max_attempts:
+            random_id = random.randint(min_id, max_id)
+            fact = db.session.query(Facts).filter(Facts.id == random_id, Facts.language == language).first()
+
+            if fact:
+                return fact
+
+            attempts += 1
+
+        return None
